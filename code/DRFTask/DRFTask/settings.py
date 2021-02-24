ca64364d97887b1e8474c67c6844323f3d57f665
sc0lnd <stomchuk34@gmail.com>
2021-02-24 17:22:44 +0200

    Dev without uwsgi

--- code/DRFTask/DRFTask/settings.py
+++ code/DRFTask/DRFTask/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = 'i-(eio-p*rh8x!uj52=@zr!--v92@)v+xta$b-g$y8a5kfqyd4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['161.35.222.60', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -69,15 +69,15 @@ WSGI_APPLICATION = 'DRFTask.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
+        'NAME': 'drftask',
         'USER': 'postgres',
-        'PASSWORD': 'example',
-        'HOST': 'localhost',
-        'PORT': '5432',
-
+        'PASSWORD': 'postgres',
+        'HOST': 'postgres',
+        'PORT': '',
     }
-
 }
+
+
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
@@ -113,5 +113,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 SHELL_PLUS = "ipython"
